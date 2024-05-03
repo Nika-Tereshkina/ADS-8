2252b1c28b1bc9b27332f9bc914de05753e9fdc4
Nika-Tereshkina <159996107+Nika-Tereshkina@users.noreply.github.com>
2024-05-04 02:29:48 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,116 +1,82 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
 Train::Train() {
-	Cage{ false, nullptr, nullptr };
-	countOp = 0;
-	first = nullptr;
+    Cage{ false, nullptr, nullptr };
+    countOp = 0;
+    first = nullptr;
 }
-void Train::addCage(bool a) {
-	if (!first) {
-		first = new Cage;
-		first->light = a;
-		first->prev = first;
-		first->next = first;
-	} else {
-		Cage* p = first;
-		while (p->next != first)
-			p = p->next;
-		Cage* tmp = new Cage;
-		tmp->light = a;
-		tmp->prev = tmp->next = nullptr;
-		tmp->prev = p;
-		p->next = tmp;
-		tmp->next = first;
-		first->prev = tmp;
-	}
+void Train::addCage(bool f) {
+    if (!first) {
+        first = new Cage;
+        first->light = f;
+        first->prev = first;
+        first->next = first;
+    } else {
+        Cage* w = first;
+        while (w->next != first)
+            w = w->next;
+        Cage* tmp = new Cage;
+        tmp->light = f;
+        tmp->prev = tmp->next = nullptr;
+        tmp->prev = w;
+        w->next = tmp;
+        tmp->next = first;
+        first->prev = tmp;
+    }
 }
 int Train::getOpCount() {
-	return countOp;
+return countOp;
 }
 int Train::getLength() {
-	int length = 0;
-	int count_steps = 0;
-	int count_steps_2 = 0;
-	Cage* h = first;
-		if (h->light == false) {
-			h->light = true;
-			h = h->next;
-			count_steps++;
-			while (true) {
-				while (h->light != true) {
-					count_steps++;
-					h = h->next;
-				}
-				h = h->prev;
-	if (h->light == false) {
-		h->light = true;
-		h = h->next;
-		count_steps++;
-		while (true) {
-			while (h->light != true) {
-				count_steps++;
-				h = h->next;
-			}
-			h = h->prev;
-			count_steps_2++;
-			while (h->light != true) {
-				count_steps_2++;
-				while (h->light != true) {
-					count_steps_2++;
-					h = h->prev;
-				}
-				if (count_steps_2 == count_steps) {
-					countOp = count_steps * 2;
-					return count_steps;
-				}
-		    }
-	} else { //if (h->light == true) {
-			h->light = false;
-			h = h->next;
-			count_steps++;
-			while (true) {
-				while (h->light != false) {
-					count_steps++;
-					h = h->next;
-				}
-				h = h->prev;
-				count_steps_2++;
-				while (h->light != false) {
-					count_steps_2++;
-					h = h->prev;
-			}
-			if (count_steps_2 == count_steps) {
-				countOp = count_steps * 2;
-				return count_steps;
-			}
-		}
-	} else {
-		int count = 0;
-		int res = 0;
-		bool end = true;
-		Cage* cur = first;
-		do {
-			cur = cur->next;
-			count++;
-			res++;
-			if (cur->light) {
-				cur->light = false;
-				for (int i = count; i > 0; i--) {
-					cur = cur->prev;
-					res++;
-				}
-				if (!cur->light) {
-					countOp = res;
-					return count;
-					end = false;
-				}
-				if (count_steps_2 == count_steps) {
-					countOp = count_steps * 2;
-					return count_steps;
-				else {
-					count = 0;
-				}
-			}
-		} while (end);
-	}
+    int length = 0;
+    int count_steps = 0;
+    int count_steps_2 = 0;
+    Cage* m = first;
+    if (m->light == false) {
+        m->light = true;
+        m = m->next;
+        count_steps++;
+        while (true) {
+            while (m->light != true) {
+                count_steps++;
+                m = m->next;
+            }
+            m = m->prev;
+            count_steps_2++;
+            while (m->light != true) {
+                count_steps_2++;
+                m = m->prev;
+            }
+            if (count_steps_2 == count_steps) {
+                countOp = count_steps * 2;
+                return count_steps;
+            }
+        }
+    } else {
+        int counter = 0;
+        int result = 0;
+        bool endl = true;
+        Cage* cureer = first;
+        do {
+            cureer = cureer->next;
+            counter++;
+            result++;
+            if (cureer->light) {
+                cureer->light = false;
+                for (int i = counter; i > 0; i--) {
+                    cureer = cureer->prev;
+                    result++;
+                }
+                if (!cureer->light) {
+                    countOp = result;
+                    return counter;
+                } else {
+                    counter = 0;
+                }
+            } else {
+                continue;
+            }
+        } while (endl);
+    }
+return 0;
 }
